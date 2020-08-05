3e24b709e8cd2b557ed92001d7c70010dbbcf723
unknown <hzh_innovation@163.com>
2020-08-05 12:16:44 +0800

    [hzh] feat: add name size valid

--- src/test/java/com/thoughtworks/rslist/api/UserControllerTest.java
+++ src/test/java/com/thoughtworks/rslist/api/UserControllerTest.java
@@ -3,7 +3,6 @@ package com.thoughtworks.rslist.api;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.thoughtworks.rslist.domain.Gender;
 import com.thoughtworks.rslist.domain.User;
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
@@ -14,7 +13,8 @@ import org.springframework.test.web.servlet.MockMvc;
 import static org.hamcrest.Matchers.hasSize;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
 @AutoConfigureMockMvc
@@ -32,5 +32,14 @@ class UserControllerTest {
 
         mockMvc.perform(get("/rs/getUserList"))
                 .andExpect(jsonPath("$.*", hasSize(1)));
+    }
+
+    @Test
+    void should_return_bad_request_when_userName_to_long() throws Exception {
+        User user = new User("zhangsan123", Gender.MALE, 20, "a@b.com", "11234567890");
+        ObjectMapper objectMapper = new ObjectMapper();
+        String userString = objectMapper.writeValueAsString(user);
+        mockMvc.perform(post("/rs/addUser").content(userString).contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isBadRequest());
     }
 }
