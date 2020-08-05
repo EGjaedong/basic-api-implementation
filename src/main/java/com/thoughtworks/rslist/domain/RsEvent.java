3c33651f3531e9a0c496ffbda5da7b39c113735b
unknown <hzh_innovation@163.com>
2020-08-05 18:50:03 +0800

    [hzh] feat: delete user in /rs/list

--- src/main/java/com/thoughtworks/rslist/domain/RsEvent.java
+++ src/main/java/com/thoughtworks/rslist/domain/RsEvent.java
@@ -1,5 +1,7 @@
 package com.thoughtworks.rslist.domain;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
@@ -54,10 +56,12 @@ public class RsEvent {
         this.keyWord = keyWord;
     }
 
+    @JsonIgnore
     public User getUser() {
         return user;
     }
 
+    @JsonProperty
     public void setUser(User user) {
         this.user = user;
     }
