8876ef17683f4f7e8351488538fb774d1c187aa9
unknown <hzh_innovation@163.com>
2020-08-06 14:55:13 +0800

    [hzh] feat: add find user by id

--- src/main/java/com/thoughtworks/rslist/service/UserService.java
+++ src/main/java/com/thoughtworks/rslist/service/UserService.java
@@ -1,7 +1,9 @@
 package com.thoughtworks.rslist.service;
 
 import com.thoughtworks.rslist.domain.User;
+import com.thoughtworks.rslist.entity.UserEntity;
 import com.thoughtworks.rslist.repositories.UserListRepository;
+import com.thoughtworks.rslist.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -10,14 +12,34 @@ import java.util.List;
 @Service
 public class UserService {
     @Autowired
-    UserListRepository userRepository;
+    UserListRepository userListRepository;
 
-    public int addOne(User user){
-        return userRepository.addUser(user);
+    @Autowired
+    UserRepository userRepository;
+
+    public int addOne(User user) {
+        return userListRepository.addUser(user);
     }
 
-    public List<User> getAllUsers(){
-        List<User> userList = userRepository.getUserList();
+    public List<User> getAllUsers() {
+        List<User> userList = userListRepository.getUserList();
         return userList;
+    }
+
+    public int register(User user) {
+        UserEntity entity = new UserEntity().builder()
+                .userName(user.getName())
+                .age(user.getAge())
+                .gender(user.getGender())
+                .email(user.getEmail())
+                .phone(user.getPhone())
+                .voteNum(user.getVoteNum())
+                .build();
+        UserEntity saved = userRepository.save(entity);
+        return saved.getId();
+    }
+
+    public UserEntity findUserById(int id) {
+        return userRepository.findUserEntityById(id);
     }
 }
